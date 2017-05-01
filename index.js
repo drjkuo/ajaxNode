22c1d7529605479a0bbcf9b645fa85522f5f0a50
drjkuo <drjkuo@gmail.com>
2017-05-01 12:35:15 -0700

    jsonp on both client and server
    Client side setting:
    Get method; key/value pair in .ajax(…,data,…); jsonCallback: do not use unless you need to change callback name
    
    Server side setting:
    Use Express middleware
    MUST: res.jsonp(enter arguments for callback function);
    
    Reference: http://expressjs.com/en/api.html#res.jsonp

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-var http = require('http');
+// var http = require('http');
 
-http.createServer(function (req, res) {
+var express = require('express');
+var app = express();
+
+// http://expressjs.com/en/api.html#res.jsonp
+app.set("jsonp callback", true);
+app.get('/', function (req, res) {
     console.log('request received');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('_testcb(\'{"message": "Hello world!"}\')');
-}).listen(3000);
+    console.log(req.query.name);
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.write(req.query.name);
+    // res.end('_testcb(\'{"message": "Hello world!!"}\')');
+    // res.end('_testcb()');
+    // res.end('_testcb(' + req.query.name + ')');
+    // res.header('Content-type','application/json');
+    res.jsonp(req.query.name);
+});
+
+app.listen(3000);
